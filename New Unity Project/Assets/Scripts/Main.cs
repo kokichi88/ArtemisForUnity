8a9dfa11432982370d5a6c54d51c7b8c8eba7248
kokichi88 <kokichi88@gmail.com>
2014-06-26 11:08:05 +0700

    init commit

---
+++ New Unity Project/Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Artemis;
+using Artemis.System;
+
+public class Main : MonoBehaviour {
+	EntityWorld world;
+	// Use this for initialization
+	void Awake () {
+		world = new EntityWorld();
+
+		for(int i = 0; i< 5; ++i){
+			Entity e = world.CreateEntity();
+			TestComp1 te = new TestComp1();
+			e.AddComponent(te);
+		}
+		EntitySystem.BlackBoard.SetEntry<TestProcessor>("test1", new TestProcessor());
+		//world.RefreshEntity();
+		TestProcessor processor = new TestProcessor();
+		TestProcessor2 processor2 = new TestProcessor2();
+		world.SystemManager.SetSystem<TestProcessor>(processor,Artemis.Manager.GameLoopType.Update);
+//		world.SystemManager.SetSystem<TestProcessor2>(processor2,Artemis.Manager.GameLoopType.Update);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		world.Update(0);
+		world.Draw();
+	}
+}
---
+++ New Unity Project/Assets/Scripts/TestComp1.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using Artemis.Interface;
+
+public class TestComp1 : IComponent 
+{
+	public int data1;
+	public int data2;
+}
